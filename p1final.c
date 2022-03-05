675e14cc1a55c57fe4623f2d5f4a54150f5271dc
Pooja <96231234+poojas2003@users.noreply.github.com>
2022-03-05 07:15:32 +0000

    p2final.c

--- p1final.c
+++ p1final.c
@@ -1,1 +1,35 @@
 #include <stdio.h>
+void input(int *num1, int *den1, int *num2, int *den2)
+{
+  printf("enter numerator and denominator for first number\n");
+  scanf("%d%d",num1,den1);
+  printf("enter numerator and denominator for second number\n");
+  scanf("%d%d",num2,den2);
+}
+void add(int num1,int den1, int num2, int den2, int *num3, int *den3,int gcd)
+{
+  int i;
+*num3=(num1*den2)+(num2*den1);
+  *den3=(den1*den2);
+  
+    for(int i=1;i<=num3 && i=den3;++i)
+      {
+        if((*num3)%i==0 && (*den3)%i==0);
+
+      gcd=i;
+        }
+  
+}
+void output(int num1, int den1, int num2, int den2, int num3, int den3)
+{
+  printf("sum of fractions %d/%d+%d/%d=%d/%d",num1,den1,num2,den2,num3,den3);
+}
+int main()
+{
+  int num1,den1,num2,den2,num3,den3,gcd ;
+  input(&num1,&den1,&num2,&den2);
+  add(num1,den1,num2,den2,&num3,&den3,gcd);
+  output(num1,den1,num2,den2,num3,den3);
+  return 0;
+}
+
--- p2final.c
+++ p2final.c
@@ -1,1 +1,51 @@
 #include<stdio.h>
+#include<math.h>
+struct _fraction
+{
+int num,den;
+};
+typedef struct _fraction Fraction;
+
+Fraction input_fraction()
+{
+  Fraction a;
+  printf("enter the number\n");
+  scanf("%d%d",&a.num,&a.den);
+  return a;
+}
+Fraction smallest_fraction(Fraction f1, Fraction f2, Fraction f3)
+{
+  int num1,den1,num2,den2,num3,den3;
+  Fraction x=den1*den2*den3;
+  
+  num1=num1*x/den1;
+   num2=num2*x/den2;
+     num3=num3*x/den3;
+    if(num1<num2 && num1<num3)
+      return num1;
+  else if(num2<num3)
+    return num2;
+  else
+    return num3; 
+}
+
+void output(Fraction f1, Fraction f2, Fraction f3, Fraction small)
+{
+if(small==num1)
+  printf("%d is small",f1);
+  else if(small==num2)
+    printf("%d is small",f2);
+  else
+    printf("%d is small",f3);
+}
+int main()
+{
+  
+  Fraction a,f1,f2,f3,small;
+  a=input_fraction();
+  
+  return small;
+  }
+  
+  
+}
