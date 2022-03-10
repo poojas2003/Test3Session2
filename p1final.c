967ceaaf9e8a779a2442af704162ae569562979a
Pooja <96231234+poojas2003@users.noreply.github.com>
2022-03-10 09:34:06 +0000

    P1final.c

--- p1final.c
+++ p1final.c
@@ -6,30 +6,31 @@ void input(int *num1, int *den1, int *num2, int *den2)
   printf("enter numerator and denominator for second number\n");
   scanf("%d%d",num2,den2);
 }
-void add(int num1,int den1, int num2, int den2, int *num3, int *den3,int gcd)
+void add(int num1,int den1, int num2, int den2, int *num3, int *den3,int *gcd)
 {
   int i;
-*num3=(num1*den2)+(num2*den1);
+  *num3=(num1*den2)+(num2*den1);
   *den3=(den1*den2);
   
-    for(int i=1;i<=num3 && i<=den3;++i)
+    for(int i=1;i<=*num3 && i<=*den3;++i)
       {
         if((*num3)%i==0 && (*den3)%i==0);
 
-      gcd=i;
+      *gcd=i;
         }
   
 }
-void output(int num1, int den1, int num2, int den2, int num3, int den3)
+void output(int num1, int den1, int num2, int den2, int num3, int den3,int gcd)
 {
-  printf("sum of fractions %d/%d+%d/%d=%d/%d",num1,den1,num2,den2,num3,den3);
+  printf("sum of two fractions is %d/%d+%d/%d=%d/%d",num1,den1,num2,den2,num3/gcd,den3/gcd);
+  printf("\n");
 }
 int main()
 {
   int num1,den1,num2,den2,num3,den3,gcd ;
   input(&num1,&den1,&num2,&den2);
-  add(num1,den1,num2,den2,&num3,&den3,gcd);
-  output(num1,den1,num2,den2,num3,den3);
+  add(num1,den1,num2,den2,&num3,&den3,&gcd);
+  output(num1,den1,num2,den2,num3,den3,gcd);
   return 0;
 }
 
